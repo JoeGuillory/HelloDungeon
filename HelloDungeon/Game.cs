c24cc457b649da82122c8ff0e34f3a3adbb8c5eb
JoeGuillory <joeguillory15@gmail.com>
2024-09-04 14:37:50 -0500

    Revamp
    added strucks

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -9,6 +9,9 @@ namespace HelloDungeon
 {
     internal class Game
     {
+        /// <summary>
+        /// Player stats
+        /// </summary>
         struct Player
         {
             public string name;
@@ -16,29 +19,65 @@ namespace HelloDungeon
             public float mana;
             public int gold;
             public float armor;
-            public float magicDamage;
-            public float meleeDamage;
+            public float damage;
             public string role;
-            public  Player(string name, float health, float mana, int gold, float armor, float magicDamage, float meleeDamage, string role)
-                    {
-                     this.name = name;
-                     this.health = health;
-                     this.mana = mana;
-                     this.gold = gold;
-                     this.armor = armor;
-                     this.magicDamage = magicDamage;
-                     this.meleeDamage = meleeDamage;
-                     this.role = role;
+            public  Player(string name, float health, float mana, int gold, float armor, float damage, string role)
+            {
+             this.name = name;
+             this.health = health;
+             this.mana = mana;
+             this.gold = gold;
+             this.armor = armor;
+             this.damage = damage;
+             this.role = role;
                    
-                    }
+            }
+        }
+        /// <summary>
+        /// Weapon blueprint
+        /// </summary>
+        struct Weapon
+        {
+            public int damage;
+            public Weapon(int damage)
+            {
+                this.damage = damage;
+            }
+        }
+        /// <summary>
+        /// Enemy bluprint
+        /// </summary>
+        struct Enemy
+        {
+            public string name;
+            public float health;
+            public float armor;
+            public float damage;
+            public Enemy(string name, float health, float armor, float damage)
+            {
+                this.name = name;
+                this.health = health;
+                this.armor = armor;
+                this.damage = damage;
+            }
         }
         public void Run()
         {
-            Player player1 = new Player("Bob", 10, 5, 3, 5, 5, 5, "");
-            
-            bool goblinEncounter = false;
-            bool isDead = false;
-            
+            // Weapons
+            Weapon wand = new Weapon(3);
+            Weapon shortStaff = new Weapon(9);
+            Weapon staff = new Weapon(12);
+            Weapon dagger = new Weapon(8);
+            Weapon sword = new Weapon(12);
+            Weapon greatsword = new Weapon(16);
+            //Enemies
+            Enemy goblin = new Enemy("Goblin", 15, 3, 4);
+
+            //Player
+            Player player1 = new Player("Scarletta", 50, 5, 3, 5, 5, "");
+            float attack = player1.damage;
+            float defend = 5;
+           
           
             Console.WriteLine("Hello, " + player1.name);
             Console.WriteLine("Welcome to my dungeon!");
@@ -54,10 +93,10 @@ namespace HelloDungeon
             if (input == 1)
             {
                     player1.role = "Warrior";
-                    player1.health += 3; 
+                    player1.health += 4; 
                     player1.mana -= 2;
                     player1.armor += 5;
-                    player1.magicDamage -= 4;
+                    player1.damage += 2;
             }
             else if (input == 2)
             {
@@ -65,7 +104,7 @@ namespace HelloDungeon
                     player1.health -= 3;
                     player1.mana += 5;
                     player1.armor -= 2;
-                    player1.meleeDamage -= 4;
+                    player1.damage += 6;
             }
 
             if (player1.role == "Wizard")
@@ -77,8 +116,7 @@ namespace HelloDungeon
                 Console.WriteLine("Mana: " + player1.mana);
                 Console.WriteLine("Gold: " + player1.gold);
                 Console.WriteLine("Player Role: " + player1.role);
-                Console.WriteLine("Magic damage: " + player1.magicDamage);
-                Console.WriteLine("Melee damage:" + player1.meleeDamage);
+                Console.WriteLine("Magic damage: " + player1.damage);
                 Console.WriteLine();
                 Console.WriteLine("Oh one more thing i forgot to tell you. It is not that important but you do not have a weapon.");
                 Console.WriteLine("You must have been an apprentice");
@@ -94,100 +132,30 @@ namespace HelloDungeon
                 Console.WriteLine("Mana: " + player1.mana);
                 Console.WriteLine("Gold: " + player1.gold);
                 Console.WriteLine("Player Role: " + player1.role);
-                Console.WriteLine("Magic damage: " + player1.magicDamage);
-                Console.WriteLine("Melee damage:" + player1.meleeDamage);
+                Console.WriteLine("Magic damage: " + player1.damage);
                 Console.WriteLine();
-                Console.WriteLine("Oh one more thing i forgot to tell you. It is not that important but you do not have a weapon."); 
-                Console.WriteLine("You must have been an apprentice");
-                Console.WriteLine("I believe you will make it out alive. *Cough* *Cough*");
+               
             }
             Console.WriteLine();
-            
+            Console.WriteLine("As you walk through your are faced with your first battle");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("A goblin has appeared");
+
+            while(goblin.health != 0 || goblin.health >=0)
+            {
+                
+
+
+
+
+
+            }
+
+
            
 
-            // Wizard Path
-            if(player1.role == "Wizard")
-            {
-
-               input = GetInput("As you enter the dungeon you run into your first option. There are two doors before you. Which do you choose.", "1 Left", "2 Right");
-
-
-                // First Option
-                if (input == 1)
-                {
-                        Console.WriteLine("You find a old wooden staff. As you knock off the dust you feel stronger");
-                        player1.magicDamage += 5;
-                        Console.WriteLine();
-                        Console.WriteLine("Magic damage: " + player1.magicDamage);
-                        
-                }
-                else if (input == 2)
-                {
-                    
-                    input = GetInput("How unlucky. You find yourself faced with a goblin. Do you coward in fear or fight it head on", "Coward in fear", "Fight is head on");
-                    goblinEncounter = true;
-                }
-                
-               
-                //Door 1
-                if (goblinEncounter == false)
-                {
-                Console.WriteLine();
-                Console.WriteLine("You take a easy stroll to the next door");
-                }
-                //Door 2
-                else if (goblinEncounter == true)
-                {
-                
-                    if (input == 1)
-                    {
-                        Console.WriteLine("As you duck in fear the golbin jumps over you and falls into a unexpected cavern that reaches deeper into the cave.");
-                        Console.WriteLine();
-                        Console.WriteLine("You hear the voice of the goblin grow more and more distance as it falls.");
-                        Console.WriteLine();
-                        Console.WriteLine("You survive the encounter");
-
-                    }
-                    else if (input == 2)
-                    {
-                        Console.WriteLine("You try your best to fight the goblin but you remembered that you have no weapons.");
-                        Console.WriteLine();
-                        Console.ReadKey();
-                        Console.WriteLine("You are defeated");
-                        Console.ReadKey();
-                        isDead = true;
-                    }
-
-                }
-                // Player death choice
-                if (isDead == true)
-                {
-                    input = GetInput("Would you like to try again", "Yes", "No");
-
-                    if (input == 1)
-                    {
-                        Console.Clear();
-                        Run();
-                    }
-                    else if (input == 2)
-                    {
-                       
-                    }
-                }
-              
-
-              // End of wizard Path
-            }
-
-
-            // Warrior Path
-            else if (player1.role == "Warrior")
-            {
-                Console.WriteLine("As you enter the dungeon you run into your first option. There are two doors before you. Which do you choose.");
-                Console.WriteLine("1 | 2");
-                
-
-            }
+            
 
 
 
@@ -196,7 +164,9 @@ namespace HelloDungeon
 
 
         }
-        // Get the input from Player
+        /// <summary>
+        /// Get a 2 option choice from Player
+        /// </summary>
         int GetInput(string description, string option1, string option2)
         {
             string input = "";
@@ -238,6 +208,102 @@ namespace HelloDungeon
 
 
         }
+        
+        /// <summary>
+        /// Input for battle encounters
+        /// </summary>
+        int GetInput(string description, string option1, string option2, string option3)
+        {
+            string input = "";
+            int inputRecieved = 0;
+            // Input loop
+            while (inputRecieved != 1 && inputRecieved != 2)
+            {
+                // Print options
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
+                Console.WriteLine("3. " + option3);
+                Console.Write("> ");
+
+                // Get input for player
+                input = Console.ReadLine();
+
+                // If player selected the first option
+                if (input == "1" || input == option1)
+                {
+                    // Set inputRecieved to be the first option
+                    inputRecieved = 1;
+                }
+                // Otherwise if the player selects the second option
+                else if (input == "2" || input == option2)
+                {
+                    inputRecieved = 2;
+                }
+                else if (input == "3" || input == option3)
+                {
+                    inputRecieved = 3;
+                }
+                //If neither are true
+                else
+                {
+                    // Display a error Message
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                }
+
+            }
+            Console.Clear();
+            return inputRecieved;
+
+
+        }
+
+        /// <summary>
+        /// Request if the player wants to try again
+        /// </summary>
+
+        int Deathrequest()
+        {
+            int request;
+            request = GetInput("Would you like to try again", "Yes", "No");
+
+            if (request == 1)
+            {
+                Console.Clear();
+                Run();
+            }
+            else if (request == 2)
+            {
+
+            }
+            return request;
+        }
+
+        int Attackrequest()
+        {
+            int input;
+            input = GetInput("Select", "Attack", "Defend", "Heal");
+
+            if (input == 1)
+            {
+
+
+            }
+            else if (input == 2)
+            {
+
+
+            }
+            else if (input == 3)
+            {
+
+
+            }
+
+            return input ;
+        }
+
 
        
 
